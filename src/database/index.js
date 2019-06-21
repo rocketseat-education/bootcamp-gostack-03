df462e12296176959ce1bd7aa615c78860d65c83
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-21 17:16:53 -0300

    4 - Migration e model de agendamento

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
