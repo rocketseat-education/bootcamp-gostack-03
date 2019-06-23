5375cff121e021ef51dc4c634f1610be21c69afd
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-23 11:01:42 -0300

    12 - Listando notificações do usuário

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,26 @@
+import User from '../models/User';
+import Notification from '../schemas/Notification';
+
+class NotificationController {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only provider can load notifications' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+}
+
+export default new NotificationController();
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -27,6 +28,8 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
