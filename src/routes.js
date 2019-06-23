52d172d161482a95a78b1c48d3bfc03c64c38f0a
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-23 11:01:54 -0300

    13 - Marcar notificações como lidas

--- src/routes.js
+++ src/routes.js
@@ -30,6 +30,7 @@ routes.post('/appointments', AppointmentController.store);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
