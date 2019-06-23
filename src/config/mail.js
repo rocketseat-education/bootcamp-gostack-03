f4ddcedf9568596295c01c3c0995a7c12479a3de
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-23 11:41:32 -0300

    15 - Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6b4604e728b554',
+    pass: '7316f39ad49262',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
