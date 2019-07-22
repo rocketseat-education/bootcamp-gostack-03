0afc55e5d43679cfb81e4b98960f4c14d8c01cb2
Diego Fernandes <diego.schell.f@gmail.com>
2019-07-22 14:17:52 -0300

    Ajustes na API

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -68,13 +69,23 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 }
