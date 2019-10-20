1092a3640806c244090b3cd0fbe7e7dfb7e94051
João Pedro Schmitz <oi@joaopedro.cc>
2019-10-20 10:05:31 -0300

    fix: Typo on function name

--- src/app/jobs/CancellationMail.js
+++ src/app/jobs/CancellationMail.js
@@ -10,7 +10,7 @@ class CancellationMail {
   async handle({ data }) {
     const { appointment } = data;
 
-    await Mail.senddMail({
+    await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
       template: 'cancellation',
